fae9b798e1f6eb09a1f1f77839a54141b79f4692
Muhammad Jaya Siraj <sirajjaya@gmail.com>
2019-02-22 01:38:44 +0700

    update dan backup

--- soal4_dekripsi.sh
+++ soal4_dekripsi.sh
@@ -1,53 +1,23 @@
-chr() {
-  [ "$1" -lt 256 ] || return 1
-  printf "\\$(printf '%03o' "$1")"
-}
-
-ord() {
-  LC_CTYPE=C printf '%d' "'$1"
-}
-
 #Rab Feb 20 17:50:59 WIB 2019
 file="$1"
+filedec="$1dec"
 jam="${file:0:2}"
-echo $jam
+#echo $jam
 
-enc=""
 
-convert() {
-  num=`ord $1`
-  #echo $num
-  if [ $num -lt 65 ] || [ $num -gt 122 ]
-  then
-  enc="$enc$1"
-  elif [ $num -ge 65 ] && [ $num -le 90 ]
-  then
-    let "num=num-64"
-    let "num=num-jam"
-    if [ $num -lt 1 ]
-    then
-      let "num=num+25"
-    fi
-    let "num=num+64"
-    enc="$enc`chr $num`"
-  #echo $num
-  elif [ $num -ge 97 ] && [ $num -le 122 ]
-  then
-    let "num=num-96"
-    let "num=num-jam"
-    if [ $num -lt 1 ]
-    then
-      let "num=num+25"
-    fi
-    let "num=num+96"
-    enc="$enc`chr $num`"
-  fi
-}
+to_change_low="`echo {a..z} | tr -d " "`"
+to_change_up="`echo {A..Z} | tr -d " "`"
+to_change_low="$to_change_low$to_change_low"
+to_change_up="$to_change_up$to_change_up"
 
-text=`cat "$file"`
-
-for (( i=0; i<${#text}; i++)); do
-  convert "${text:$i:1}"
+for (( i=0; i<26; i++)); do
+    dec="$dec${to_change_low:$i-jam:1}"
 done
 
-`echo "$enc" > "$file"`
+for (( i=0; i<26; i++)); do
+    dec="$dec${to_change_up:$i-jam:1}"
+done
+
+to_change="`echo {a..z} | tr -d " "``echo {A..Z} | tr -d " "`"
+
+`tr $dec $to_change < "$file" > "$filedec"`
