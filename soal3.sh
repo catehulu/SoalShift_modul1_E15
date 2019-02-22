667ee6a854a5150ce871fadb9cc689488f8f2c02
catehulu <sirajjaya@gmail.com>
2019-02-22 14:17:17 +0700

    update no 3

--- soal3.sh
+++ soal3.sh
@@ -13,15 +13,12 @@ do
    let "ptr++"
   else
    #echo "Tidak!"
-   number="${current:8:-4}"
    gate=0
   fi
 done
-count=1
+
 password=`< /dev/urandom tr -dc a-z0-9 | head -c1 >> $check`
-while [ $count -le 5 ]
-do
-password=`< /dev/urandom tr -dc A-Za-z0-9 | head -c2 >> $check`
-let "count++"
-done
+
+password=`< /dev/urandom tr -dc A-Za-z0-9 | head -c10 >> $check`
+
 password=`< /dev/urandom tr -dc 0-9 | head -c1 >> $check`
